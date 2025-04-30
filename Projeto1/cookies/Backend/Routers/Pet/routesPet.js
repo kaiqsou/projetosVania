3633a13fb3ccbb92abd97f1ca1d7cf8968b1c5af
Kaique <kaiqsouza2004@hotmail.com>
2025-04-30 09:30:03 -0300

    att

---
+++ Projeto1/cookies/Backend/Routers/Pet/routesPet.js
@@ -0,0 +1,8 @@
+import{Router} from "express";
+import PetController from "../../controllers/PetController.js";
+import verifyToken from "../../helpers/verify-token.js";
+import imageUpload from "../../helpers/image-upload.js";
+const routesPet = Router();
+routesPet.post('/Create',verifyToken, imageUpload.array("imagens"), PetController.create);
+routesPet.get('/getAll', PetController.getAll);
+export default routesPet;
